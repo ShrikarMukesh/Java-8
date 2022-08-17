1cf8a18cc88b6115fc751faeb2259603fb11c3bb
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2022-08-17 23:09:21 +0530

    Added pdf file

--- READEME.md
+++
@@ -1,7 +0,0 @@
-Java 8 Features
-1) Lambda Expression
-2) Functional Interface
-3) Streams
-4) Optional Class
-5) Date and Time API
-6) 
--- streams/src/main/java/streams1/StreamsExamples.java
+++ streams/src/main/java/streams1/StreamsExamples.java
@@ -6,13 +6,21 @@ import data.StudentDataBase;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 
 public class StreamsExamples {
     public static void main(String[] args) {
           //student name and there activities in map
-        Map<String, List<String>> nameAndActivities =  StudentDataBase.getAllStudents().stream()
+        Predicate<Student> studentGradePredicate = student -> student.getGradeLevel() >= 3;
+        Predicate<Student> studentGpaPredicate = student -> student.getGpa() > 3.9;
+
+        Map<String, List<String>> nameAndActivities =  StudentDataBase.
+                getAllStudents()
+                .stream()
+                .filter(studentGradePredicate)
+                .filter(studentGpaPredicate)
                 .collect(Collectors.toMap(Student::getName, Student::getActivities));
 
         for(Map.Entry<String, List<String>> stringListEntry : nameAndActivities.entrySet()){
